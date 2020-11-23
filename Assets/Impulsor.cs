3d79f3f2278f48a74e1fae0af4757358e48899fe
BaconWarrior <urieloc.yea@gmail.com>
2020-11-22 21:34:07 -0600

    Se agrega decoracion al nivel

--- Assets/Impulsor.cs
+++ Assets/Impulsor.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Impulsor : MonoBehaviour
 {
+    public Text tipoDeChoque;
+    public PhysicMaterial rubber;
     Rigidbody rb;
     Rigidbody rbObjetivo;
     Collider colisionador;
@@ -15,7 +18,20 @@ public class Impulsor : MonoBehaviour
     private void Awake()
     {
         tipoChoque = Random.Range(1, 4);
+        //tipoChoque = 2;
         objetivo = GameObject.FindGameObjectWithTag("objetivo");
+        if (tipoChoque == 1)
+        {
+            tipoDeChoque.text = "Tipo de Choque: Elastico";
+        }
+        else if (tipoChoque == 2)
+        {
+            tipoDeChoque.text = "Tipo de Choque: Inelastico";
+        }
+        else if (tipoChoque == 3)
+        {
+            tipoDeChoque.text = "Tipo de Choque: Plastico";
+        }
     }
     void Start()
     {
@@ -26,16 +42,22 @@ public class Impulsor : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Debouncer"))
+        {
+            colisionador.material = null;
+        }
         if (other.CompareTag("Choque"))
         {
             if (tipoChoque == 1)
             {
+                colisionador.material = rubber;
                 rbObjetivo.velocity = (rb.velocity * -1);
             }
             else if (tipoChoque == 2)
             {
+                colisionador.material = rubber;
                 rb.mass = (rbObjetivo.mass / 10);
-                blocker = Instantiate(blocker, posSpawn);
+                blocker = Instantiate(blocker, posSpawn.position, transform.rotation);
                 other.gameObject.SetActive(false);
             }
             else if (tipoChoque == 3)
